1d7d5b9b869d6d6eaae2829ca11266d3b5cc0ba4
Daniel Neyla <daniel.neyla@gmail.com>
2023-10-30 17:27:55 +0000

    Now the user can place a bet

--- slot_machine.py
+++ slot_machine.py
@@ -4,6 +4,7 @@ import random
 class SlotMachine:
     def __init__(self):
         self.balance = 0
+        self.bet = 0
 
     def deposite(self):
         while True:
@@ -15,7 +16,21 @@ class SlotMachine:
                 else:
                     print("Deposite has to be between 0 and 21")
             except ValueError:
-                print("You have to enter a valid number")
+                print("Enter a valid number")
+
+    def bet(self):
+        while True:
+            try:
+                bet = int(input("How much do you want to bet? "))
+                if bet < self.balance:
+                    self.bet += bet
+                    break
+                else:
+                    print(
+                        "Deposite has to be greater than 0 and smaller than your current balance"
+                    )
+            except ValueError:
+                print("Enter a valid number")
 
 
 # Usage
