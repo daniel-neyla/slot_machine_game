66fe35bed21f3f84455ffe080c02d73c76b1e317
Daniel Neyla <daniel.neyla@gmail.com>
2023-10-30 18:27:39 +0000

    Did some changes. Now the user can choose if he wants to place a bet or deposit money etc.

--- slot_machine.py
+++ slot_machine.py
@@ -1,36 +1,40 @@
 import random
 
+MIN_BET = 5
+MAX_BET = 100
+MAX_DEPOSIT = 1000
+
 
 class SlotMachine:
     def __init__(self):
         self.balance = 0
-        self.bet = 0
+        self.current_bet = 0
 
-    def deposite(self):
+    def get_valid_input(self, prompt, min_value, max_value):
         while True:
             try:
-                deposite = int(input("How much do you want to bet? "))
-                if deposite > 0 and deposite <= 20:
-                    self.balance += deposite
-                    break
+                value = int(input(prompt))
+                if not min_value <= value <= max_value:
+                    print(f"Value has to be between {min_value} and {max_value}")
                 else:
-                    print("Deposite has to be between 0 and 21")
+                    return value
             except ValueError:
                 print("Enter a valid number")
+
+    def deposit(self):
+        amount = self.get_valid_input(
+            "How much do you want to deposit? ", 1, MAX_DEPOSIT
+        )
+        self.balance += amount
 
     def bet(self):
-        while True:
-            try:
-                bet = int(input("How much do you want to bet? "))
-                if bet < self.balance:
-                    self.bet += bet
-                    break
-                else:
-                    print(
-                        "Deposite has to be greater than 0 and smaller than your current balance"
-                    )
-            except ValueError:
-                print("Enter a valid number")
+        amount = self.get_valid_input("How much do you want to bet? ", MIN_BET, MAX_BET)
+        if amount > self.balance:
+            print(
+                f"Bet cannot be bigger than your current balance, which is {self.balance}"
+            )
+        else:
+            self.current_bet += amount
 
     def withdraw(self):
         amount = self.balance
@@ -41,6 +45,39 @@ class SlotMachine:
             )
         )
 
+    def spin(self):
+        return
+
 
 # Usage
+
 slot_machine = SlotMachine()
+
+
+def game():
+    user_choices = {
+        1: slot_machine.deposit,
+        2: slot_machine.bet,
+        3: slot_machine.spin,
+        4: slot_machine.withdraw,
+        5: "exit",
+    }
+    while True:
+        print(
+            "1. Make a deposit\n2. Place a bet\n3. Spin\n4. Withdraw the money\n5. Stop playing"
+        )
+        choice = input("What do you want to do(1-5)? ")
+        if choice.isdigit():
+            choice = int(choice)
+            if choice in user_choices:
+                if user_choices[choice] == "exit":
+                    break
+                else:
+                    user_choices[choice]()
+            else:
+                print("This is not a valid choice")
+        else:
+            print("Enter a valid number")
+
+
+game()
