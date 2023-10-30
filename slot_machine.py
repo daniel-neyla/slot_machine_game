e912ca58779908114290a2d371944161ee99cf71
Daniel Neyla <daniel.neyla@gmail.com>
2023-10-30 17:22:46 +0000

    Now the user can the deposite money to the slot machine

--- slot_machine.py
+++ slot_machine.py
@@ -5,6 +5,18 @@ class SlotMachine:
     def __init__(self):
         self.balance = 0
 
+    def deposite(self):
+        while True:
+            try:
+                deposite = int(input("How much do you want to bet? "))
+                if deposite > 0 and deposite <= 20:
+                    self.balance += deposite
+                    break
+                else:
+                    print("Deposite has to be between 0 and 21")
+            except ValueError:
+                print("You have to enter a valid number")
+
 
 # Usage
 slot_machine = SlotMachine()
