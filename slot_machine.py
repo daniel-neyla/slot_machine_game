d50ed1652676c51b243940d916426fbe41e7c0c2
Daniel Neyla <daniel.neyla@gmail.com>
2023-10-31 22:20:54 +0000

    The programm checks now how many winning combination a player has

--- slot_machine.py
+++ slot_machine.py
@@ -7,7 +7,7 @@ ROWS = 3
 COLOUMNS = 3
 MAX_LINES = 3
 
-symbols = {
+symbols_and_probabilities = {
     "Cherry": 0.3,
     "Lemon": 0.2,
     "Orange": 0.15,
@@ -16,7 +16,39 @@ symbols = {
     "Bar": 0.1,
     "Seven": 0.05,
 }
+symbols = list(symbols_and_probabilities.keys())
+probabilities = list(symbols_and_probabilities.values())
 longest_symbol = max(symbols, key=len)
+
+
+def check_vertical(reels):
+    count = 0
+    for row in reels:
+        if all(symbol == row[0] for symbol in row):
+            count += 1
+    return count
+
+
+def check_horizontal(reels):
+    count = 0
+    for s1, s2, s3 in zip(*reels):
+        if s1 == s2 == s3:
+            count += 1
+
+    return count
+
+
+def check_diagonal(reels):
+    count = 0
+    first_row, second_row, third_row = reels[0], reels[1], reels[2]
+
+    if first_row[0] == second_row[1] == third_row[2]:
+        count += 1
+
+    if first_row[2] == second_row[1] == third_row[0]:
+        count += 1
+
+    return count
 
 
 class SlotMachine:
@@ -24,7 +56,7 @@ class SlotMachine:
         self.balance = 0
         self.current_bet = 0
         self.lines = 1
-        self.reels = [list(symbols.keys()), list(symbols.keys()), list(symbols.keys())]
+        self.reels = [symbols] * ROWS
 
     def get_valid_input(self, prompt, min_value, max_value):
         while True:
@@ -42,7 +74,7 @@ class SlotMachine:
             "How much do you want to deposit? ", 1, MAX_DEPOSIT
         )
         print(
-            "You have successfully deposited {amount} to your balance".format(
+            "You have successfully deposited {amount}$ to your balance".format(
                 amount=amount
             )
         )
@@ -85,12 +117,22 @@ class SlotMachine:
     def spin(self):
         if self.current_bet < self.balance:
             spins = [
-                random.choices(reel, weights=list(symbols.values()), k=3)
+                random.choices(reel, weights=probabilities, k=COLOUMNS)
                 for reel in self.reels
             ]
+
+            print(spins)
             for i in range(3):
                 padded_symbols = [spin.ljust(len(longest_symbol)) for spin in spins[i]]
                 print("|".join(padded_symbols))
+
+            wins = 0
+            if self.lines == 3:
+                wins += check_diagonal(spins)
+                wins += check_horizontal(spins)
+                wins += check_vertical(spins)
+
+            print(wins)
 
 
 # Usage
